e3b952fe87e85c5d224aa2fd0f3737e5f891d4aa
Yousif <yousifadil16@gmail.com>
2023-08-15 05:35:07 -0700

    putchart

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,13 @@
 #include "main.h"
+
+/**
+ * main - prints Putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+
 int main(void)
 {
 	_putchar('_');
@@ -10,5 +19,6 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-	return(0);
+
+	return (0);
 }
